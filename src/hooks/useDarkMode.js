b2abdd42783af249063be407c46b80525b18be29
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-25 04:41:49 -0400

    mvp done

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,8 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [someValue, setSomeValue] = useLocalStorage("darkMode");
+  return [someValue, setSomeValue];
+};
+
+export default useDarkMode;
