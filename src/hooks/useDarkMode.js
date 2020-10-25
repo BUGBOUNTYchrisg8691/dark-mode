c698766c4b769c6607d94716244626172cf9cea5
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-10-25 18:57:47 -0400

    color scheme preference functionality for stretch done

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,6 +1,9 @@
 import useLocalStorage from "./useLocalStorage";
 
 const useDarkMode = () => {
+  if (window.matchMedia("(prefers-color-scheme: dark)")) {
+    localStorage.setItem("darkMode", true);
+  }
   const [someValue, setSomeValue] = useLocalStorage("darkMode");
   return [someValue, setSomeValue];
 };
